33c9e34a38ba922d1a0d053f0b653b4189023bb7
Jared Farkas <jaredmfarkas@gmail.com>
2019-01-18 12:47:55 -0800

    add better handling for a tie

--- js/scripts.js
+++ js/scripts.js
@@ -32,7 +32,36 @@ $(document).ready(function() {
       $("body").addClass("red");
     }else{
       //Tie
+      //check which values are highest
+      $(".tie").toggle();
+      var high = 0;
+      for(var j=0;j<arr.length;j++)
+      {
+        if (parseInt(arr[j]) >= high){
+          high = arr[j];
+        }
+      }
       $(".result").text("you're too indecisive, take the quiz again!");
+      //add all ties to result string.
+      for(j=0;j<arr.length;j++)
+      {
+        if (parseInt(arr[j]) === high){
+          switch(j){
+          case 0:
+          $(".tie p").append("CSS<br>");
+            break;
+          case 1:
+          $(".tie p").append("Java<br>");
+            break;
+          case 2:
+          $(".tie p").append("PHP<br>");
+            break;
+          default:
+          $(".tie p").append("Ruby<br>");
+            break;
+          }
+        }
+      }
     }
     console.log($("#name").val());
     $(".result").prepend($("#name").val() + ", ");
