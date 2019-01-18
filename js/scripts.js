ad67a01d1dc2ca9495c787efb89ea9fc16d637ba
Jared Farkas <jaredmfarkas@gmail.com>
2019-01-18 11:20:09 -0800

    Clean up code and text

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var arr=[0,0,0,0];
-    for (i = 0; i < 6; i++) {
-      arr[parseInt($("#ans" + (i+1)).val()-1)] +=1;
+    //6 questions, 6 loop iterations
+    for (i = 1; i <= 6; i++) {
+      arr[parseInt($("#ans" + (i)).val()-1)] +=1;
     }
     $("#formOne").toggle();
     $("button.hidden").show();
@@ -11,32 +12,30 @@ $(document).ready(function() {
 
     if(arr[0] > arr[1] && arr[0] > arr[2] && arr[0] > arr[3]){
       //C# is highest
-      $(".result").text("You should study C#");
+      $(".result").text("you should study C#");
       $(".c").toggle();
       $("body").addClass("purple");
     }else if(arr[1] > arr[0] && arr[1] > arr[2] && arr[1] > arr[3]){
       //Java is highest
-      $(".result").text("You should study Java");
+      $(".result").text("you should study Java");
       $(".java").toggle();
       $("body").addClass("white");
     }else if(arr[2] > arr[0] && arr[2] > arr[1] && arr[2] > arr[3]){
       //PHP is highest
-      $(".result").text("You should study PHP");
+      $(".result").text("you should study PHP");
       $(".php").toggle();
       $("body").addClass("blue");
-    } else if(arr[3] > arr[0] && arr[3] > arr[1] && arr[3] > arr[2]){
+    }else if(arr[3] > arr[0] && arr[3] > arr[1] && arr[3] > arr[2]){
       //Ruby is highest
-      $(".result").text("You should study Ruby");
+      $(".result").text("you should study Ruby");
       $(".ruby").toggle();
       $("body").addClass("red");
-    } else{
+    }else{
       //Tie
-      $(".result").text("You're too indecisive, take the quiz again!");
+      $(".result").text("you're too indecisive, take the quiz again!");
     }
     console.log($("#name").val());
-$(".result").prepend($("#name").val() + ", ");
-
-
+    $(".result").prepend($("#name").val() + ", ");
     event.preventDefault();
   });
   $("button.hidden").click(function(){
